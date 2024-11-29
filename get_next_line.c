c644380c35fce93111f9da82d338f7119d807c7c
Alessandro Bonanno <abonanno@c1r6p8.42firenze.it>
2024-11-29 18:55:53 +0100

    Ci siamo vicini commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: abonanno <abonanno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 16:28:32 by abonanno          #+#    #+#             */
-/*   Updated: 2024/11/29 15:20:05 by abonanno         ###   ########.fr       */
+/*   Updated: 2024/11/29 18:53:28 by abonanno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,92 +15,123 @@
 #include <stdio.h>
 #include <string.h>
 
-char	*get_next_line(int fd)
+static char	*initialize_and_allocate(char **str)
 {
-    static char	*save;
-    char		*str;
-    char		*chr;
-    ssize_t		rd;
-    ssize_t		rd_total;
-
-	if(fd < 0)
-		return NULL;
-    str = ft_calloc(1, BUFFER_SIZE + 1);
-    if(str == NULL)
-        return (NULL);
-    if (save != NULL)
-    {
-		if(str)
-            free(str);
-        str = ft_strdup(save);
-        if(str == NULL)
-            return (NULL);
-        free(save);
-        save = NULL;
-    }
-    chr = ft_strchr(str, '\n');
-    if(chr != NULL)
-    {
-        if(save)
-            free(save);
-        save = ft_strdup(&chr[1]);
-        if(save == NULL)
-            return (NULL);
-        chr[1] = '\0';
-        return (str);
-    }
-    rd_total = ft_strlen(str);
-    str = ft_realloc(str, BUFFER_SIZE + rd_total + 1);
-    if(str == NULL)
-        return (NULL);
-    rd = read(fd, &str[rd_total], BUFFER_SIZE);
-	rd_total += rd;
-    str = ft_realloc(str, BUFFER_SIZE + rd_total + 1);
-    while(rd > 0)
-    {
-        chr = ft_strchr(str, '\n');
-        if(chr != NULL)
-        {
-			if(save)
-            	free(save);
-            save = ft_strdup(&chr[1]);
-            chr[1] = '\0';
-			str = ft_realloc(str, ft_strlen(str) + 1);
-            return (str);
-        }
-        rd = read(fd, &str[rd_total], BUFFER_SIZE);
-        rd_total += rd;
-        str = ft_realloc(str, BUFFER_SIZE + rd_total + 1);
-        if(str == NULL)
-            return (NULL);
-    }
-	if (rd == 0 && str[0] != '\0')
-	{
-		if(save)
-			free(save);
-		save = NULL;
-		str = ft_realloc(str, ft_strlen(str) + 1);
-		return (str);
-	}
-	if(rd == -1 || rd == 0)
-	{
-		free(str);
+	*str = calloc(BUFFER_SIZE, 1);
+	if (*str == NULL)
 		return (NULL);
-	}
-	free(str);
-    return NULL;
+	return (*str);
 }
 
-// int main()
-// {
-//     int fd = open("fd.txt", O_RDONLY);
-//     char *str = get_next_line(fd);
-//     while (str != NULL)
-//     {
-//         printf("%s", str);
-//         free(str);
-//         str = get_next_line(fd);
-//     }
-// 	free(str);
-//     close(fd);
-// }
+static char	*handle_saved_data(char **save, char **str)
+{
+	char	*chr;
+
+	if (*save != NULL)
+	{
+		free(*str);
+		*str = ft_strdup(*save);
+		if (*str == NULL)
+			return (NULL);
+		free(*save);
+		*save = NULL;
+	}
+	chr = ft_strchr(*str, '\n');
+	if (chr != NULL)
+	{
+		*save = ft_strdup(&chr[1]);
+		if (*save == NULL)
+			return (NULL);
+		chr[1] = '\0';
+		return (*str);
+	}
+	return (NULL);
+}
+
+static char	*read_from_fd(int fd, char **str, char **save, ssize_t rd_total)
+{
+	ssize_t	rd;
+	char	*chr;
+
+	rd = read(fd, &(*str)[rd_total], BUFFER_SIZE);
+	rd_total += rd;
+	*str = ft_realloc(*str, BUFFER_SIZE + rd_total + 1);
+	while (rd > 0)
+	{
+		chr = ft_strchr(*str, '\n');
+		if (chr != NULL)
+		{
+			*save = ft_strdup(&chr[1]);
+			chr[1] = '\0';
+			*str = ft_realloc(*str, ft_strlen(*str) + 1);
+			return (*str);
+		}
+		rd = read(fd, &(*str)[rd_total], BUFFER_SIZE);
+		rd_total += rd;
+		*str = ft_realloc(*str, BUFFER_SIZE + rd_total + 1);
+		if (*str == NULL)
+			return (NULL);
+	}
+	return (NULL);
+}
+
+static char	*finalize_result(char **str, char **save, ssize_t rd)
+{
+	if (rd == 0 && (*str)[0] != '\0')
+	{
+		if (*save)
+			free(*save);
+		*save = (NULL);
+		*str = ft_realloc(*str, ft_strlen(*str) + 1);
+		return (*str);
+	}
+	if (rd == -1 || rd == 0)
+	{
+		free(*str);
+		return (NULL);
+	}
+	free(*str);
+	return (NULL);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*save;
+	char		*str;
+	char		*result;
+	ssize_t		rd_total;
+
+	if (fd < 0)
+		return (NULL);
+	str = initialize_and_allocate(&str);
+	if (str == NULL)
+		return (NULL);
+	result = handle_saved_data(&save, &str);
+	if (result != NULL)
+		return (result);
+	rd_total = ft_strlen(str);
+	str = ft_realloc(str, BUFFER_SIZE + rd_total + 1);
+	if (str == NULL)
+		return (NULL);
+	result = read_from_fd(fd, &str, &save, rd_total);
+	if (result != NULL)
+		return (result);
+	return (finalize_result(&str, &save, 0));
+}
+
+int main(void)
+{
+	int fd;
+	char *line;
+
+	fd = open("fd.txt", O_RDONLY);
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("%s\n", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (0);
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: abonanno <abonanno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 18:57:45 by abonanno          #+#    #+#             */
-/*   Updated: 2024/11/29 14:13:52 by abonanno         ###   ########.fr       */
+/*   Updated: 2024/11/29 18:54:40 by abonanno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,146 +16,111 @@
 
 size_t	ft_strlen(const char *s)
 {
-    size_t	i;
-
-    i = 0;
-    while (s[i])
-    {
-        i++;
-    }
-    return (i);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
-{
 	size_t	i;
 
 	i = 0;
-	if (size > 0)
+	while (s[i])
 	{
-		while ((i < size - 1) && (src[i]))
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = 0;
+		i++;
 	}
-	return (ft_strlen(src));
-}
-
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
-{
-    size_t	i;
-    size_t	dst_len;
-
-    dst_len = ft_strlen(dst);
-    i = 0;
-    if (size > dst_len)
-    {
-        while ((i < size - dst_len - 1) && (src[i]))
-        {
-            dst[dst_len + i] = src[i];
-            i++;
-        }
-        dst[dst_len + i] = 0;
-    }
-    return (dst_len + ft_strlen(src));
+	return (i);
 }
 
 char	*ft_strchr(const char *s, int c)
 {
-    char	ch;
-    int		i;
+	char	ch;
+	int		i;
 
-    i = 0;
-    ch = (char) c;
-    while (s[i])
-    {
-        if (s[i] == ch)
-            return ((char *)&s[i]);
-        i++;
-    }
-    if (s[i] == ch)
-        return ((char *)&s[i]);
-    return (NULL);
+	i = 0;
+	ch = (char) c;
+	while (s[i])
+	{
+		if (s[i] == ch)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == ch)
+		return ((char *)&s[i]);
+	return (NULL);
 }
 
 char	*ft_strdup(const char *s)
 {
-    char	*ptr;
-    size_t	len_s;
+	char	*ptr;
+	size_t	len_s;
+	size_t	i;
 
-    len_s = ft_strlen(s);
-    ptr = (char *) malloc(sizeof(char) * (len_s + 1));
-    if (ptr == NULL)
-        return (NULL);
-    ft_strlcpy(ptr, s, len_s + 1);
-    return (ptr);
+	len_s = ft_strlen(s);
+	ptr = (char *) malloc(sizeof(char) * (len_s + 1));
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	if (len_s + 1 > 0)
+	{
+		while ((i < len_s) && (s[i]))
+		{
+			ptr[i] = s[i];
+			i++;
+		}
+		ptr[i] = '\0';
+	}
+	return (ptr);
 }
 
 void	*ft_bzero(void *str, unsigned long int n)
 {
-    unsigned long int	i;
-    unsigned char		*ptr;
+	unsigned long int	i;
+	unsigned char		*ptr;
 
-    i = 0;
-    ptr = (unsigned char *)str;
-    while (i < n)
-    {
-        ptr[i] = '\0';
-        i++;
-    }
-    return (str);
-}
-
-void	*ft_calloc(size_t nmemb, size_t size)
-{
-    void	*ptr;
-
-    ptr = (void *) malloc(nmemb * size);
-    if (ptr == NULL)
-        return (NULL);
-    ft_bzero(ptr, nmemb * size);
-    return (ptr);
+	i = 0;
+	ptr = (unsigned char *)str;
+	while (i < n)
+	{
+		ptr[i] = '\0';
+		i++;
+	}
+	return (str);
 }
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-    size_t			i;
-    unsigned char	*i_dest;
-    unsigned char	*i_src;
+	size_t			i;
+	unsigned char	*i_dest;
+	unsigned char	*i_src;
 
-    i_dest = (unsigned char *) dest;
-    i_src = (unsigned char *) src;
-    i = 0;
-    while (i < n && i_src[i])
-    {
-        i_dest[i] = i_src[i];
-        i++;
-    }
-    return (dest);
+	i_dest = (unsigned char *) dest;
+	i_src = (unsigned char *) src;
+	i = 0;
+	while (i < n && i_src[i])
+	{
+		i_dest[i] = i_src[i];
+		i++;
+	}
+	return (dest);
 }
-
 void *ft_realloc(void *ptr, size_t size)
 {
-    void *new_ptr;
-    if(ptr == NULL && size == 0)
-        return NULL;
-    if(ptr == NULL && size != 0)
-        return ft_calloc(1, size);
-    if(ptr != NULL && size == 0)
-    {
-        free(ptr);
-        return (NULL);
-    }
-    new_ptr = ft_calloc(1, size);
-    if(new_ptr == 0)
-        return NULL;
-    if(ptr != NULL && size != 0)
-    {
-        ft_memcpy(new_ptr, ptr, size);
-        free(ptr);
-        return new_ptr;
-    }
-    return NULL;
+	void *new_ptr;
+	if(ptr == NULL && size == 0)
+		return NULL;
+	if(ptr == NULL && size != 0){
+		new_ptr = malloc(size);
+		return ft_bzero(new_ptr, size);
+	}
+	if(ptr != NULL && size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_ptr = malloc(size);
+	new_ptr = ft_bzero(new_ptr, size);
+	if(new_ptr == 0)
+		return NULL;
+	if(ptr != NULL && size != 0)
+	{
+		ft_memcpy(new_ptr, ptr, size);
+		free(ptr);
+		return new_ptr;
+	}
+	return NULL;
 }
